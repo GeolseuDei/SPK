11684bef94417063302c5c17986f0d8b7d9fe18f
pratamaaudi <pratamaaudi@gmail.com>
2016-12-01 13:24:06 +0700

    Update 4.1
    - penambahan judul inputan
    - merapihkan
    - tambahan tombol cancel dan tombol confirm

--- signup.php
+++ signup.php
@@ -7,21 +7,65 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body style="background-color: #00E5FF">
-        <div class="row">
-            
+        <div class="row" style="margin-top: 10%;">
+
             <div class="col-md-3"></div>
             <div class="col-md-6" style="background-color: white;">
-                
-                <input type="text"/>
-                <input type="password"/>
-                <input type="text"/>
-                <select>
-                    
-                    <option value="a">a</option>
-                    <option value="a">a</option>
-                    <option value="a">a</option>
-                    <option value="a">a</option>
-                </select>
+
+                <div class="row">
+
+                    <div class="col-md-3" style="text-align: right;">
+
+                        USERNAME :
+                    </div>
+                    <div class="col-md-9">
+
+                        <input type="text" style="width: 100%;"/>
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-md-3" style="text-align: right;">
+
+                        PASSWORD :
+                    </div>
+                    <div class="col-md-9">
+
+                        <input type="password" style="width: 100%;"/>
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-md-3" style="text-align: right;">
+
+                        NAMA :
+                    </div>
+                    <div class="col-md-9">
+
+                        <input type="text" style="width: 100%;"/>
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-md-3" style="text-align: right;">
+
+                        KOTA :
+                    </div>
+                    <div class="col-md-9">
+
+                        <select>
+
+                            <option value="a">a</option>
+                            <option value="a">a</option>
+                            <option value="a">a</option>
+                            <option value="a">a</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <button type="button" class="btn btn-success" data-dismiss="modal" style="float: right; margin-right: 10px;">CONFIRM</button>
+                    <button type="button" class="btn btn-warning" data-dismiss="modal" style="float: right; margin-right: 10px;">CANCEL</button>
+                </div>
             </div>
             <div class="col-md-3"></div>
         </div>
