3259b47d4f568478c41c7542202fa25e995f946a
pratamaaudi <pratamaaudi@gmail.com>
2016-12-01 13:14:49 +0700

    Update 4
    - halaman sign up
    - desain posisi

---
+++ signup.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>SIGN UP</title>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body style="background-color: #00E5FF">
+        <div class="row">
+            
+            <div class="col-md-3"></div>
+            <div class="col-md-6" style="background-color: white;">
+                
+                <input type="text"/>
+                <input type="password"/>
+                <input type="text"/>
+                <select>
+                    
+                    <option value="a">a</option>
+                    <option value="a">a</option>
+                    <option value="a">a</option>
+                    <option value="a">a</option>
+                </select>
+            </div>
+            <div class="col-md-3"></div>
+        </div>
+    </body>
+</html>
