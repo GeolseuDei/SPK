a6244425cd1b00d73e53f467676a5b6f1b95a3d1
pratamaaudi <pratamaaudi@gmail.com>
2016-12-01 13:49:29 +0700

    Update 5
    - halaman guest
    - persiapan isi konten

---
+++ guest.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GUEST</title>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body style="background-color: #2196F3;">
+        <p>TOP 10 LIST</p>
+        <div class="row">
+            
+            <div class="col-md-1"></div>
+            <div class="col-md-10" style="background-color: yellow;">
+                
+                <div class="row">
+                    
+                    <div class="col-md-4" style="text-align: center;">
+                        NAMA WISATA
+                    </div>
+                    <div class="col-md-4" style="text-align: center;">
+                        JENIS WISATA
+                    </div>
+                    <div class="col-md-4" style="text-align: center;">
+                        RATING
+                    </div>
+                </div>
+                <div>
+                    DESKRIPSI
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+    </body>
+</html>
