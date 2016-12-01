b8c9cf51be605d1fe84ac213906cca669de3c06e
pratamaaudi <pratamaaudi@gmail.com>
2016-12-01 14:17:34 +0700

    Update 6.1
    - form input wisata baru

--- user.php
+++ user.php
@@ -7,8 +7,9 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
+        
         <div class="row">
-            
+
             <div class="col-md-3">
                 <button type="button" class="btn btn-success" data-dismiss="modal">INDONESIA</button>
             </div>
@@ -22,13 +23,100 @@
                 <button type="button" class="btn btn-success" data-dismiss="modal">CARI</button>
             </div>
         </div>
+        <div>
+
+            <div class="row">
+
+                <div class="col-md-2" style="text-align: right;">
+
+                    Nama :
+                </div>
+                <div class="col-md-9">
+
+                    <input type="text" style="width: 100%;"/>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-md-2" style="text-align: right;">
+
+                    Alamat :
+                </div>
+                <div class="col-md-9">
+
+                    <input type="text" style="width: 100%;"/>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-md-2" style="text-align: right;">
+
+                    Kota :
+                </div>
+                <div class="col-md-9">
+
+                    <select>
+
+                        <option value="a">a</option>
+                        <option value="a">a</option>
+                        <option value="a">a</option>
+                        <option value="a">a</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-md-2" style="text-align: right;">
+
+                    Kategori :
+                </div>
+                <div class="col-md-9">
+
+                    <select>
+
+                        <option value="a">a</option>
+                        <option value="a">a</option>
+                        <option value="a">a</option>
+                        <option value="a">a</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-md-2" style="text-align: right;">
+
+                    Deskripsi :
+                </div>
+                <div class="col-md-9">
+
+                    <textarea rows="5" cols="50"></textarea>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-md-2" style="text-align: right;">
+
+                    Ranking :
+                </div>
+                <div class="col-md-9">
+
+                    <select>
+
+                        <option value="a">a</option>
+                        <option value="a">a</option>
+                        <option value="a">a</option>
+                        <option value="a">a</option>
+                    </select>
+                </div>
+            </div>
+        </div>
         <div class="row">
-            
+
             <div class="col-md-1"></div>
             <div class="col-md-10" style="background-color: yellow;">
-                
+
                 <div class="row">
-                    
+
                     <div class="col-md-4" style="text-align: center;">
                         NAMA WISATA
                     </div>
