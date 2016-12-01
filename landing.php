e381ae76a8a4c50d55f5fc45a3b4230c718a8858
pratamaaudi <pratamaaudi@gmail.com>
2016-12-01 12:46:57 +0700

    Update 3.1
    - menghilangkan tombol X pada modal

--- landing.php
+++ landing.php
@@ -59,7 +59,6 @@ and open the template in the editor.
                     
                     <div class="modal-header">
                         
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
                         <h4 class="modal-title">LOGIN</h4>
                     </div>
                     <div class="modal-body">
