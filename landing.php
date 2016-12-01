0888d11f4941b378918a69f5beb0cbe24ec5f249
pratamaaudi <pratamaaudi@gmail.com>
2016-12-01 12:19:26 +0700

    Update 1
    - tambah tulisan welcome
    - kerangka login, sign up, dan guest
    - tema background

--- landing.php
+++ landing.php
@@ -7,11 +7,42 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Landing</title>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
+    <body style="background-color: #78909C">
+        
+        <div class="row" style="background-color: white;">
+            
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+                
+                <h2 style="text-align: center;">Welcome To SPK - Pariwisata</h2>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+        <div class="row" style="margin-top: 15%;">
+            
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                
+                <div class="row" style="background-color: white;">
+                    
+                    <div class="col-md-5" style="background-color: yellow; width: 40%; text-align: center;">
+                        
+                        <h6>LOGIN</h6>
+                    </div>
+                    <div class="col-md-2"></div>
+                    <div class="col-md-5" style="background-color: yellow; width: 40%; text-align: center;">
+                        
+                        <h6>GUEST</h6>
+                    </div>
+                    <p style="margin-top: 10px; display: inline-block;">ga punya akun ? segera sign up disini!!</p>
+                </div>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
     </body>
 </html>
